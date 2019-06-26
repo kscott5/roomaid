b1322c5b736dbb037d6133c83634152a0e35a56d
Karega Khinua Scott <kkscott@outlook.com>
2019-06-26 16:56:54 -0500

    Paginated room list and simples UI styling.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -12,10 +12,60 @@ const locales = {
             save: 'Save',
             edit: 'Edit',
             name: 'Enter room name',
-            description: 'Enter room description'
+            description: 'Enter room description',
+            page: 'Pages',
+            search: {
+                count: 'Display'
+            },
+            theaders: {
+                name: 'Name',
+                description: 'Description',
+            }
         }
     }
 };
+
+const list_room_template = `
+<div>
+    <label>{{labels.search.count}}</label>
+    <div class="select is-rounded">
+        <select id="viewCount" size="1">
+            <option value="5 default">5</option>
+            <option value="10">10</option>
+            <option value="15">15</option>
+        </select>
+    </div>
+    <table class="table">
+        <thead>
+            <th>{{labels.theaders.name}}</th>
+            <th>{{labels.theaders.description}}</th>
+            <th>&nbsp;</th>
+        </thead>
+        <tbody>
+            <tr v-for="room in pagination.documents" v-bind:key="room.id" v-on:mouseover="toggle(room)" v-on:mouseout="toggle(room)" v-bind:id="room.id">
+                <td>
+                    {{room.name}}
+                </td>
+                <td>
+                    {{room.description}}                        
+                </td>
+                <td>
+                    <button class="button is-rounded" v-on:click="editRoom(room)">{{labels.edit}}</button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    <div v-show="pagination.pageCount > 0">
+        <nav class="pagination is-rounded" role="navigation" aria-label="pagination">
+            <p>{{labels.page}} {{pagination.pageIndex}} of {{pagination.pageCount}}.
+            <a class="pagination-previous" v-on:click="showPagination(pagination.pageIndex-1)">Previous</a>
+            <a class="pagination-next" v-on:click="showPagination(pagination.pageIndex+1)">Next page</a>
+            <!--input class="input is-rounded" type="number" v-bind:min="1" v-bind:value="pagination.pageIndex" v-bind:max="pagination.pageCount"-->
+        </nav>
+    </div>
+</div>`;
+
+
 const edit_room_template = `
 <form>
     <div class="field">
@@ -122,35 +172,24 @@ const editroom = Vue.component('edit-room', {
 });
 
 const listrooms = Vue.component('list-rooms', {
-    template:`
-        <table class="table">
-            <tbody>
-                <tr v-for="room in rooms" v-bind:key="room.id" v-on:mouseover="toggle(room)" v-on:mouseout="toggle(room)" v-bind:id="room.id">
-                    <td>
-                        {{room.name}}
-                    </td>
-                    <td>
-                        {{room.description}}                        
-                    </td>
-                    <td>
-                        <button v-on:click="editRoom(room)">{{labels.edit}}</button>
-                    </td>
-                </tr>
-            </tbody>
-        </table>`,
+    template: list_room_template,
         data: function() {
             return {
-                labels: locales.labels["en-US"],
-                rooms: {}
+                labels: locales.labels["en-US"],                
+                pagination: {
+                    documents: {},
+                    pageCount: 0,
+                    pageIndex: 0
+                }
             };
         },
         created: function() {
-            var vm = this.$data;
+            var data = this.$data;
 
             this.$http.get("/api/room")
                 .then(function(response) {
                     console.log(response);
-                    vm.rooms = response.data;
+                    data.pagination = response.data;
                 });
         },
         methods: {
